1ac57c11a135ba3f6e935f64661c5ddfeacffdf3
ReptilianEye <piotr.rzadkowski@outlook.com>
2024-05-29 13:27:54 +0200

    old server marked as depricated

---
+++ server_old/models/shop.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class Shop(BaseModel):
+    name: str
+    longitude: float
+    latitude: float
+    flavors: list[str]
+    card_payment: bool = False
+    time: list[int]
+    startTime: str
+    endTime: str
---
+++ server_old/schema/schemas.py
@@ -0,0 +1,68 @@
+from distance_calc import distance
+
+from config.database import init_db
+
+from models.shop import Shop
+from models.user import User
+from bson import ObjectId
+
+# longitude: str # x coordinate
+#     latitude: str # y coordinate
+#     user_id: str # id of the seller
+#     flavors: str # enum variants separated by semicolons
+#     payment_methods: str = 0 # 0 - cash, 1 - card, 2 - cash and card
+
+
+class Repository:
+    def __init__(self) -> None:
+        self.shops, self.users = init_db()
+
+    # def invidual_serial(shop) -> dict:
+    #     return {
+    #         "id": str(shop["_id"]),
+    #         "name": shop["name"],
+    #         "longitude": float(shop["longitude"]),
+    #         "latitude": float(shop["latitude"]),
+    #         "card_payment": shop["card_payment"],
+    #         "flavors": shop["flavors"],
+    #         "time": shop["time"],
+    #         "startTime": shop["startTime"],
+    #         "endTime": shop["endTime"]
+    #     }
+    def mapToShop(self, shop: dict) -> dict:
+        # shop["id"] = str(shop["_id"])
+        # del shop["_id"]
+        return shop
+        
+
+    def get_all_shops(self) -> list[dict]:
+        shops_coursor = self.shops.find()
+        print(shops_coursor[0])
+        shops = list(map(self.mapToShop,shops_coursor ))
+        return shops
+
+    def get_shop_by_id(self, id) -> dict:
+        return self.mapToShop(self.shops.find_one({"_id": ObjectId(id)}))
+
+    def insert_shop(self, shop: Shop):
+        self.shops.insert_one(shop.dict())
+
+    def list_serial(self, shops: list[dict]) -> list:
+        serialized = {}
+        for shop in shops:
+            serialized[shop["id"]] = (shop["latitude"], shop["longitude"])
+        return serialized
+
+    def filter_by_distance(self, shops, localization, radius=5):
+        return self.get_shops_data(distance(self.list_serial(shops), localization, radius=radius))
+
+    def filter_n_nearest(self, shops, localization, n):
+        return self.get_shops_data(distance(self.list_serial(shops), localization, number_of_output=n))
+
+    def get_shops_data(self, filtered_shops):
+        list_of_shops = []
+        for id, distance in filtered_shops:
+            shop = self.get_shop_by_id(id)
+            shop["distance"] = distance
+            list_of_shops.append(shop)
+        return list_of_shops
