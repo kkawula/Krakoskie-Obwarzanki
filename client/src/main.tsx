bc79505c57ccdfb266657973d041d4fcb0ff64d2
ReptilianEye <ReptilianEye@users.noreply.github.com>
2024-06-19 18:04:27 +0000

    style fixes by prettier

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import {ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
