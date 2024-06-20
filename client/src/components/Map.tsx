e3ef9a61df6edc39b5ea9628a20d191b7c9289bb
ReptilianEye <piotr.rzadkowski@outlook.com>
2024-06-20 20:44:43 +0200

    testing if ruff not working for front

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -55,6 +55,7 @@ export default function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
       <MarkerSetter.Provider value={setNewMarker}>
         <LocationMarker />
       </MarkerSetter.Provider>
