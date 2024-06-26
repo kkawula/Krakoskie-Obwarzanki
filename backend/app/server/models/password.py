aa4b2749203f86e1950fa35548099c16f1edfacf
stylub <stylub0@gmail.com>
2024-06-26 20:24:50 +0200

    removed obsolete code

--- backend/app/server/models/password.py
+++
@@ -1,9 +0,0 @@
-from beanie import Document, PydanticObjectId
-
-
-class Password(Document):
-    user_id: PydanticObjectId
-    hashed_password: str
-
-    class Settings:
-        name = "passwords"
